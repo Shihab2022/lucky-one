af31b92d379fef4c86a19ab292535cfab002b8aa
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-03-25 23:13:01 +0600

    use useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  
+  const [data,setData]=useState([])
+
+  useEffect(() =>{
+
+fetch('fakeData.json')
+.then(response =>response.json())
+.then(data=>setData(data))
+
+
+  },[])
+  console.log(data)
   return (
     <div className="App">
     
